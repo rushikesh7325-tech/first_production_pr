b9da8a11d013d59c489c400c7f1a2d4b08a266ce
Om Tawade <om.tawade@dypic.in>
2026-02-07 14:52:54 +0530

    Add files via upload

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'routes/app_routes.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'POSH Policy',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
+      initialRoute: AppRoutes.poshHome,
+      routes: AppRoutes.routes,
+    );
+  }
+}
